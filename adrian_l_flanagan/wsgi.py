0b8456055c59b2c487666b699ac248257488a500
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2018-08-06 11:09:18 -0400

    initial commit

---
+++ adrian_l_flanagan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for adrian_l_flanagan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adrian_l_flanagan.settings")
+
+application = get_wsgi_application()
