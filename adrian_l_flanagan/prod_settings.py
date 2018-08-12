9dc3eb2ef192bcbf4e95285b6590372172149177
Adrian L. Flanagan <a.lloyd.flanagan@gmail.com>
2018-08-12 18:08:17 -0400

    Create apps from old project

--- adrian_l_flanagan/prod_settings.py
+++ adrian_l_flanagan/prod_settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about_me',
+    'validator',
+    'website'
 ]
 
 MIDDLEWARE = [
