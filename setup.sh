ba7d1b3629115fd44ea044e9b38d213923334459
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-31 14:44:42 -0400

    set up db access

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# clone auth config (might need to fix source path for another version)
+sudo cp /etc/postgresql/9.6/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
+
 cd adrian_l_flanagan || exit 1
 ln -s prod_settings.py settings.py
 cd .. || exit 1
