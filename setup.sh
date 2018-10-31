25d94d8210029099ed7f081905f7c99ee9760848
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-31 15:23:43 -0400

    run psql as postgres

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-# clone auth config (might need to fix source path for another version)
-sudo cp /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf || exit 1
-# really? Why above stil not fix my problem?
-sudo md5sum /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
+# clone auth config
+sudo cp /etc/postgresql/9.6/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf || exit 1
 cd adrian_l_flanagan || exit 1
 ln -s prod_settings.py settings.py
 cd .. || exit 1
-psql -U postgres -c "CREATE USER adrian_l_flanagan PASSWORD 'travis_test_password' CREATEDB;"
+sudo -u postgres psql -U postgres -c "CREATE USER adrian_l_flanagan PASSWORD 'travis_test_password' CREATEDB;"
+python3 -m pip install --upgrade pip
 python3 -m pip install pipenv
 pipenv install --three
 pipenv run python manage.py migrate --noinput
