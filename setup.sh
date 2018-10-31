290a3b02d23d755203f37b2083f394d571be6156
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-31 15:19:10 -0400

    use sudo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # clone auth config (might need to fix source path for another version)
 sudo cp /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf || exit 1
 # really? Why above stil not fix my problem?
-md5sum /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
+sudo md5sum /etc/postgresql/9.4/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
 cd adrian_l_flanagan || exit 1
 ln -s prod_settings.py settings.py
 cd .. || exit 1
