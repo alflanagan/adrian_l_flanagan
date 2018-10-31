7fdd0e965fa4d4f2665c2771be2d344852f03163
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-31 15:42:24 -0400

    remove extraneous stuff, add comments

--- setup.sh
+++ setup.sh
@@ -4,17 +4,17 @@ PG10CONFIG=/etc/postgresql/10/main/pg_hba.conf
 
 # clone auth config
 sudo cp ${PG9CONFIG} ${PG10CONFIG} || exit 1
-echo 'local   all             adrian_l_flanagan                       md5' >> ${PG10CONFIG}
-sudo cat ${PG10CONFIG}
 sudo service postgresql restart 10
-whoami
-ll -d adrian_l_flanagan
-cd adrian_l_flanagan || exit 1
-ln -s prod_settings.py settings.py
-cd .. || exit 1
+
+# create test user
 sudo -u postgres psql -U postgres -c "CREATE USER adrian_l_flanagan PASSWORD 'travis_test_password' CREATEDB;"
+
+# setup pipenv and install dependencies
 python3 -m pip install --upgrade pip
 python3 -m pip install pipenv
 pipenv install --three
+
+# setup django environment
+ln -s prod_settings.py adrian_l_flanagan/settings.py
 pipenv run python manage.py migrate --noinput
 pipenv run python manage.py collectstatic --noinput
