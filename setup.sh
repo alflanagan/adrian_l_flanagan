8c9bd6ab293c3055bedab2ec8031d62efc4238d4
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-31 14:56:16 -0400

    Still trying on db access

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # clone auth config (might need to fix source path for another version)
-sudo cp /etc/postgresql/9.6/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
+cp /etc/postgresql/9.2/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf || exit 1
 
 cd adrian_l_flanagan || exit 1
 ln -s prod_settings.py settings.py
